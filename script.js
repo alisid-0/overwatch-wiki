b5c16fc445865af4ca72d286e0cc30d827ce9808
Ali Siddiqui <alisiddiquia22@yahoo.com>
2023-05-19 17:38:06 -0500

    added scroll animations

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const observer = new IntersectionObserver((i)=>{
+    i.forEach((j) =>{
+        console.log(j)
+        if (j.isIntersecting){
+            j.target.classList.add(`show`)
+        } else {
+            j.target.classList.remove(`show`)
+        }
+    })
+})
+
+
+const hiddenElements = document.querySelectorAll(`.hidden`)
+hiddenElements.forEach((i)=> observer.observe(i))
+
+
+
+
 const $buttonHeroes = $(`#heroes-button`)
 const $input = $(`#hero-search`)
 const $inputButton = $(`#hero-search-button`)
